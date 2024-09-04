d1d959690f2fb6ead0ccc896e99e8f103b60402a
luchomoreno <lucho.moreno@live.com>
2024-09-04 04:30:40 -0300

    [update] sphinx config with documentation

--- docs/conf.py
+++ docs/conf.py
@@ -3,25 +3,43 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../'))
+
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'UTN Python Intermediate'
-copyright = '2024, Nahuel Demian Fanego Paz / Cristian Luciano Moreno'
-author = 'Nahuel Demian Fanego Paz / Cristian Luciano Moreno'
+project = 'PYTHON-INTERMEDIATE'
+copyright = '2024, Nahuel Damian Fanego Paz / Cristian Luciano Moreno'
+author = 'Nahuel Damian Fanego Paz / Cristian Luciano Moreno'
+
+version = '0.0.1'
+release = '0.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.todo',
+]
 
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-language = 'es'
+language = 'en'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+# -- Options for todo extension ----------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/todo.html#configuration
+
+todo_include_todos = True
